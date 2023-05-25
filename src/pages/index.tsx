d6f8f0165e237642c6369c087098a5b5e2bcaa1b
Jeremy Cruz <jeremy.sunidu@gmail.com>
2023-05-25 07:44:23 -0700

    Add click and drag functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,71 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import React, { useState } from 'react';
-
 import { api } from "~/utils/api";
+import React, { useState, useEffect } from 'react';
 
 const Sequencer: React.FC = () => {
-  const [steps, setSteps] = useState<boolean[][]>([
-    Array(16).fill(false),
-    Array(16).fill(false),
-    Array(16).fill(false),
-    Array(16).fill(false),
-  ]);
+  const [channelStates, setChannelStates] = useState<Map<number, boolean[]>>(new Map());
 
-  const handleClick = (channel: number, stepIndex: number) => {
-    const updatedSteps = [...steps];
-    if (!updatedSteps[channel]) {
-      updatedSteps[channel] = [];
-    }
-    updatedSteps[channel][stepIndex] = !updatedSteps[channel][stepIndex];
-    setSteps(updatedSteps);
+  const handleStepClick = (channelIndex: number, stepIndex: number) => {
+    const updatedChannelStates = new Map(channelStates);
+    const channelSteps = updatedChannelStates.get(channelIndex) || [];
+    channelSteps[stepIndex] = !channelSteps[stepIndex];
+    updatedChannelStates.set(channelIndex, channelSteps);
+    setChannelStates(updatedChannelStates);
   };
+
+  const [mouseIsDown, setMouseIsDown] = useState<boolean>(false);
+
+  const handleStepMouseDown = (channelIndex: number, stepIndex: number) => {
+    setMouseIsDown(true);
+    handleStepClick(channelIndex, stepIndex);
+  };
+
+  const handleStepMouseEnter = (channelIndex: number, stepIndex: number) => {
+    if (mouseIsDown) {
+      handleStepClick(channelIndex, stepIndex);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setMouseIsDown(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Initialize the channel states
+    const initialChannelStates = new Map<number, boolean[]>();
+    initialChannelStates.set(0, Array<boolean>(16).fill(false));
+    initialChannelStates.set(1, Array<boolean>(16).fill(false));
+    initialChannelStates.set(2, Array<boolean>(16).fill(false));
+    initialChannelStates.set(3, Array<boolean>(16).fill(false));
+    setChannelStates(initialChannelStates);
+  }, []);
 
   return (
     <div className="sequencer">
-      {steps.map((channel, channelIndex) => (
-        <div className="channel" key={channelIndex}>
-          {channel.map((step, stepIndex) => (
+      {Array.from(channelStates.entries()).map(([channelIndex, steps]) => (
+        <div key={channelIndex} className="channel">
+          {steps.map((step, stepIndex) => (
             <React.Fragment key={stepIndex}>
             {stepIndex !== 0 && stepIndex % 4 === 0 && (
               <div className="column-separator">
                 <div className="separator-line" />
               </div>
-              // <div className="vertical-line"></div>
             )}
             <button
               className={`step ${step ? 'active' : ''}`}
-              onClick={() => handleClick(channelIndex, stepIndex)}
-            />
+              onMouseDown={() => handleStepMouseDown(channelIndex, stepIndex)}
+              onMouseEnter={() => handleStepMouseEnter(channelIndex, stepIndex)}
+            ></button>
           </React.Fragment>
           ))}
         </div>
@@ -46,12 +74,4 @@ const Sequencer: React.FC = () => {
   );
 };
 
-const App: React.FC = () => {
-  return (
-    <div className="app">
-      <Sequencer />
-    </div>
-  );
-};
-
-export default App;
+export default Sequencer;
